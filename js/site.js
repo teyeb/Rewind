ec376dc98f2781352580cf409a1b120772b44ae9
Teye Brown <helpteye@gmail.com>
2021-08-27 04:04:30 -0500

    Updated model to ensure user inputs a string of length > 1, and also checked to see if the result is a palindrome

--- js/site.js
+++ js/site.js
@@ -4,38 +4,40 @@
 const getString = ()=>{
     document.getElementById("alert").classList.add("invisible");
     const string = document.getElementById("inputReverse").value;
-    return splitString(string)
+
+    if(string.trim().length<2){
+        return alert("The input string should be longer than 1!")
+    }
+    return reverseString(string)
 }
 
-
-// split into an array
-//model 1
-const splitString = string=>{
-    const array = string.split("");
-    return reverseString(array);
-}
+ 
 
 //concat a new string in reverse
-//model2
-const reverseString = array=>{
+//model 
+const reverseString = string=>{
     let reversedString = "";
 
     //read array in reverse and concat into string
-    for(let i = array.length-1; i>=0; i--){
-        reversedString += array[i];
+    for(let i = string.length-1; i>=0; i--){
+        reversedString += string[i];
     }
 
-    return displayMessage(reversedString);
+    return displayMessage(reversedString,string);
 }
 
 //display message with reversed string
 //view
-const displayMessage = reversedString =>{
+const displayMessage = (reversedString,original) =>{
     //grab id of display message paragraph element
     const paragraph = document.getElementById("msg");
 
     //make it visible
 
     document.getElementById("alert").classList.remove("invisible");
+
+    if(original.trim() === reversedString.trim()){
+        return paragraph.innerHTML = `Your reversed string is ${reversedString} and it is a Palindrome-- same in reverse`; 
+    }
     return paragraph.innerHTML = `Your reversed string is ${reversedString}`;
 }
